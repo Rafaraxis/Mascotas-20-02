3e3627b7292f45311ced3116ed5edc1066033847
Alumno <ulsa@Pol.local>
2019-02-20 18:12:18 -0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.default')
+@section('titulo_pagina','Mascotas | Crear mascota')
+@section('titulo','Mascotas')
+@section('subtitulo','Crear mascota')
+@section('contenido')
+
+<div class="row">
+        <div class="col-md-12">
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Tabla de lista de mascotas</h3>
+                </div>
+                <div class="box-body">
+                    El contenido de mi dashboard
+                </div>
+            </div>
+        </div>
+                     </div>
+@endsection
